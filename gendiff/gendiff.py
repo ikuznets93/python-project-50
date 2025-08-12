9fb7d2c2a80c4f8a15ed37be9639014395a9bcf3
Ivan <ikuznets93@gmail.com>
2025-08-12 22:39:33 +0800

    add test

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -10,7 +10,7 @@ def generate_diff(file_path1, file_path2):
     data1 = json_parsing(file_path1)
     data2 = json_parsing(file_path2)
 
-    keys = data1.keys() | data2.keys()
+    keys = sorted(data1.keys() | data2.keys())
     result = {}
     for key in keys:
         if key not in data1:
@@ -23,4 +23,4 @@ def generate_diff(file_path1, file_path2):
             result[f'- {key}'] = data1[key]
             result[f'+ {key}'] = data2[key]
     str_result = '\n'.join(f'{k}: {v}' for k, v in result.items())
-    return print(str_result)
+    return str_result
---
+++ tests/test_gendiff.py
@@ -0,0 +1,20 @@
+from gendiff.gendiff import generate_diff
+from pathlib import Path
+
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / 'test_data' / filename
+
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+
+def test_gendiff():
+    file1_path = get_test_data_path('testfile1.json')
+    file2_path = get_test_data_path('testfile2.json')
+    actual = generate_diff(file1_path, file2_path)
+    expected = read_file('expected_result.txt')
+
+    assert actual == expected
+
